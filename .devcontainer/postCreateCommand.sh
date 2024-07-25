964f82f224d72234f262815d388c6c79171132db
Tine Kondo <kondotine@gmail.com>
2024-07-25 21:32:32 +0000

    chore(repo): improve the `devcontainer` setup
    - respect `pnpm` version declared in root `package.json`
    - improve performances when installing node modules
    - add troubleshooting instruction in `CONTRIBUTING.md` to help solve comme issues related to `GLIBC` version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+# Update the underlying (Debian) OS, to make sure we have the latest security patches and libraries like 'GLIBC' 
+echo "⚙️ Updating the underlying OS..."
+sudo apt-get update  && sudo apt-get -y upgrade
+
+# Give 'node' user access to 'node_modules' folder
+sudo chown node node_modules
+
+# Uninstall globally installed PNPM (appropriate version will be installed with corepack)
+npm uninstall -g pnpm
+
+# Prevent corepack from prompting user before downloading PNPM 
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
+# Enable corepack 
+sudo corepack enable 
+
+# Install the PNPM version defined in the root package.json
+echo "⚙️ Installing PNPM..."
+sudo corepack prepare --activate
+
 # Install dependencies
+echo "⚙️ Installing NPM dependencies..."
 pnpm install --frozen-lockfile
 
